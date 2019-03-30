4a7293474e072ba8ff42dfa4ee9cc97a73f365ad
Dileep Nandanam <dileep@Dileeps-MacBook-Air.local>
2019-03-30 03:50:55 -0700

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,8 @@
 # set path to application
 app_dir = File.expand_path("../../..", __FILE__)
+root_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+working_directory root_dir
 
 
 # Set unicorn options
